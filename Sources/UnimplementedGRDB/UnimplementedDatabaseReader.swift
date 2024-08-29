b4ec1ff9767a3810c12df557b98100ec5f6bd3a4
Rhys Morgan <rhysm@me.com>
2024-08-29 19:36:41 +0100

    A much simpler implementation, thanks to @groue!

--- Sources/UnimplementedGRDB/UnimplementedDatabaseReader.swift
+++ Sources/UnimplementedGRDB/UnimplementedDatabaseReader.swift
@@ -8,51 +8,32 @@
 import IssueReporting
 @preconcurrency import GRDB
 
-public final class UnimplementedDatabaseReader: DatabaseReader {
-  public let configuration: Configuration = IssueReporting.unimplemented(
-    "UnimplementedDatabaseReader.configuration",
-    placeholder: Configuration()
-  )
+// A type that provides read-only access to a database
+public protocol DatabaseReaderProvider {
+  /// Provides read-only access to the database.
+  @MainActor var reader: any DatabaseReader { get throws }
+}
 
-  public let path: String = IssueReporting.unimplemented("UnimplementedDatabaseReader.path", placeholder: "")
+public struct UnimplementedDatabaseReaderProvider: DatabaseReaderProvider {
+  public init() {}
 
-  public func read<T>(_ value: (Database) throws -> T) throws -> T {
-    throw UnimplementedFailure(description: "UnimplementedDatabaseReader.read")
-  }
-
-  public func unsafeRead<T>(_ value: (GRDB.Database) throws -> T) throws -> T {
-    throw UnimplementedFailure(description: "UnimplementedDatabaseReader.unsafeRead")
-  }
-
-  public func close() throws {
-    throw UnimplementedFailure(description: "UnimplementedDatabaseReader.close")
-  }
-
-  public func interrupt() {
-    IssueReporting.unimplemented("UnimplementedDatabaseReader.interrupt")
-  }
-
-  public func asyncRead(_ value: @escaping (Result<Database, any Error>) -> Void) {
-    value(.failure(UnimplementedFailure(description: "UnimplementedDatabaseReader.asyncRead")))
-  }
-
-  public func asyncUnsafeRead(_ value: @escaping (Result<Database, any Error>) -> Void) {
-    value(.failure(UnimplementedFailure(description: "UnimplementedDatabaseReader.asyncUnsafeRead")))
-  }
-
-  public func unsafeReentrantRead<T>(_ value: (Database) throws -> T) throws -> T {
-    throw UnimplementedFailure(description: "UnimplementedDatabaseReader.unsafeReentrantRead")
-  }
-
-  public func _add<Reducer: ValueReducer>(
-    observation: ValueObservation<Reducer>,
-    scheduling scheduler: some ValueObservationScheduler,
-    onChange: @escaping (Reducer.Value) -> Void
-  ) -> AnyDatabaseCancellable {
-    IssueReporting.unimplemented("UnimplementedDatabaseReader._add", placeholder: AnyDatabaseCancellable { })
+  public var reader: any DatabaseReader {
+    get throws {
+      throw UnimplementedFailure(description: "UnimplementedDatabaseReaderProvider.reader")
+    }
   }
 }
 
-public extension DatabaseReader where Self == UnimplementedDatabaseReader {
+public extension DatabaseReaderProvider where Self: DatabaseReader {
+  @MainActor var reader: any DatabaseReader { self }
+}
+
+extension AnyDatabaseReader: DatabaseReaderProvider {}
+extension DatabaseQueue: DatabaseReaderProvider {}
+extension DatabasePool: DatabaseReaderProvider {}
+extension DatabaseSnapshot: DatabaseReaderProvider {}
+extension DatabaseSnapshotPool: DatabaseReaderProvider {}
+
+public extension DatabaseReaderProvider where Self == UnimplementedDatabaseReaderProvider {
   static var unimplemented: Self { Self() }
 }
--- Sources/UnimplementedGRDB/UnimplementedDatabaseWriter.swift
+++ Sources/UnimplementedGRDB/UnimplementedDatabaseWriter.swift
@@ -8,80 +8,30 @@
 import IssueReporting
 @preconcurrency import GRDB
 
-public final class UnimplementedDatabaseWriter: DatabaseWriter {
-  public let configuration: Configuration = IssueReporting.unimplemented(
-    "UnimplementedDatabaseWriter.configuration",
-    placeholder: Configuration()
-  )
+// A type that provides write access to a database
+public protocol DatabaseWriterProvider {
+  /// Provides read/write access to the database.
+  @MainActor var writer: any DatabaseWriter { get throws }
+}
 
-  public let path: String = IssueReporting.unimplemented("UnimplementedDatabaseWriter.path", placeholder: "")
+public struct UnimplementedDatabaseWriterProvider: DatabaseWriterProvider {
+  public init() {}
 
-  public func read<T>(_ value: (Database) throws -> T) throws -> T {
-    throw UnimplementedFailure(description: "UnimplementedDatabaseWriter.read")
-  }
-
-  public func unsafeRead<T>(_ value: (GRDB.Database) throws -> T) throws -> T {
-    throw UnimplementedFailure(description: "UnimplementedDatabaseWriter.unsafeRead")
-  }
-
-  public func close() throws {
-    throw UnimplementedFailure(description: "UnimplementedDatabaseWriter.close")
-  }
-
-  public func interrupt() {
-    IssueReporting.unimplemented("UnimplementedDatabaseWriter.interrupt")
-  }
-
-  public func asyncRead(_ value: @escaping (Result<Database, any Error>) -> Void) {
-    value(.failure(UnimplementedFailure(description: "UnimplementedDatabaseWriter.asyncRead")))
-  }
-
-  public func asyncUnsafeRead(_ value: @escaping (Result<Database, any Error>) -> Void) {
-    value(.failure(UnimplementedFailure(description: "UnimplementedDatabaseWriter.asyncUnsafeRead")))
-  }
-
-  public func unsafeReentrantRead<T>(_ value: (Database) throws -> T) throws -> T {
-    throw UnimplementedFailure(description: "UnimplementedDatabaseWriter.unsafeReentrantRead")
-  }
-
-  public func _add<Reducer: ValueReducer>(
-    observation: ValueObservation<Reducer>,
-    scheduling scheduler: some ValueObservationScheduler,
-    onChange: @escaping (Reducer.Value) -> Void
-  ) -> AnyDatabaseCancellable {
-    IssueReporting.unimplemented("UnimplementedDatabaseWriter._add", placeholder: AnyDatabaseCancellable { })
-  }
-
-  public func barrierWriteWithoutTransaction<T>(_ updates: (GRDB.Database) throws -> T) throws -> T {
-    throw UnimplementedFailure(description: "UnimplementedDatabaseWriter.barrierWriteWithoutTransaction")
-  }
-
-  public func asyncBarrierWriteWithoutTransaction(_ updates: @escaping (Result<Database, any Error>) -> Void) {
-    updates(.failure(UnimplementedFailure(description: "UnimplementedDatabaseWriter.asyncBarrierWriteWithoutTransaction")))
-  }
-
-  public func asyncWriteWithoutTransaction(_ updates: @escaping (Database) -> Void) {
-    IssueReporting.unimplemented("UnimplementedDatabaseWriter.asyncWriteWithoutTransaction")
-  }
-
-  public func spawnConcurrentRead(_ value: @escaping (Result<Database, any Error>) -> Void) {
-    value(.failure(UnimplementedFailure(description: "UnimplementedDatabaseWriter.spawnConcurrentRead")))
-  }
-
-  // MARK: - Currently Unimplementable methods
-  public func writeWithoutTransaction<T>(_ updates: (Database) throws -> T) rethrows -> T {
-    fatalError("Impossible to implement writeWithoutTransaction on UnimplementedDatabaseWriter")
-  }
-
-  public func unsafeReentrantWrite<T>(_ updates: (Database) throws -> T) rethrows -> T {
-    fatalError("Impossible to implement unsafeReentrantWrite on UnimplementedDatabaseWriter")
-  }
-
-  public func concurrentRead<T>(_ value: @escaping (Database) throws -> T) -> DatabaseFuture<T> {
-    fatalError("Impossible to implement concurrentRead on UnimplementedDatabaseWriter")
+  public var writer: any DatabaseWriter {
+    get throws {
+      throw UnimplementedFailure(description: "UnimplementedDatabaseWriterProvider.writer")
+    }
   }
 }
 
-public extension DatabaseWriter where Self == UnimplementedDatabaseWriter {
+public extension DatabaseWriterProvider where Self: DatabaseWriter {
+  @MainActor var writer: any DatabaseWriter { self }
+}
+
+extension AnyDatabaseWriter: DatabaseReaderProvider {}
+extension DatabaseQueue: DatabaseWriterProvider {}
+extension DatabasePool: DatabaseWriterProvider {}
+
+public extension DatabaseWriterProvider where Self == UnimplementedDatabaseWriterProvider {
   static var unimplemented: Self { Self() }
 }
